f2f3d17d1406faabbd979196fc56eb9991085f58
Ba (Baker) V. NGUYEN <baker@enclave.vn>
2015-08-17 09:39:14 +0700

    first commit

---
+++ TheFirstProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheFirstProject
+//
+//  Created by Ba (Baker) V. NGUYEN on 8/17/15.
+//  Copyright (c) 2015 Ba (Baker) V. NGUYEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheFirstProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheFirstProject
+//
+//  Created by Ba (Baker) V. NGUYEN on 8/17/15.
+//  Copyright (c) 2015 Ba (Baker) V. NGUYEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
